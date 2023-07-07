7fe142c7b7c7c22e2dc851f6ddd8d29c25a0eed8
Heavenaef <dd20100123@163.com>
2023-07-07 20:57:14 +0800

    新文件：   main.cpp 	新文件：   programs/calc 	新文件：   programs/solvex

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "programs/solvex"
+using namespace std;
+string com;
+int main() {
+  while (com != "exit") {
+    printf("mathcmd> ");
+    cin >> com;
+    if (com == "solvex")
+      solvex();
+  }
+  return 0;
+}
